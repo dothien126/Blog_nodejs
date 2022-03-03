2e864714eed353113bf451e4ce35ba63f2a29021
dothien126 <dothien2601ak39@gmail.com>
2022-03-04 00:17:42 +0700

    Query string parameters

--- src/index.js
+++ src/index.js
@@ -22,6 +22,11 @@ app.get("/", (req, res) => {
    res.render("home");
 });
 
+app.get("/search", (req, res) => {
+  console.log(req.query.q);
+  res.render("search");
+});
+
 app.get("/news", (req, res) => {
   res.render("news");
 });
