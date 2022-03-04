8506f263e644320e154722c5f069eec627cf77e3
dothien126 <dothien2601ak39@gmail.com>
2022-03-05 00:08:52 +0700

    Use prettier for formatter code

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,15 @@
-const res = require("express/lib/response");
+const res = require('express/lib/response');
 
 class SiteController {
-
     // [GET] /
     index(req, res) {
-        res.render("home");
-    };
+        res.render('home');
+    }
 
     // [GET] /search
     search(req, res) {
-        res.render("search");
-    };
+        res.render('search');
+    }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require("./routes")
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // middlewear
-app.use(express.urlencoded({
-  extended: true
-}));  // gui dang form
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+); // gui dang form
 
 // gui dang XMLHttpRequest, fetch, axios
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
-app.engine("hbs", engine({
-  extname: ".hbs"
-}));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
 route(app);
 
-
-app.listen(port, () => console.log("He he he he"));
+app.listen(port, () => console.log('He he he he'));
