91088e7b5316d5e6077b1455a83cdab8d3187df7
dothien126 <dothien2601ak39@gmail.com>
2022-03-04 22:55:47 +0700

    config Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes")
+
 app.use(express.static(path.join(__dirname, "public")));
 // middlewear
 app.use(express.urlencoded({
@@ -25,21 +27,8 @@ app.engine("hbs", engine({
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-   res.render("home");
-});
+// Route init
+route(app);
 
-app.get("/search", (req, res) => {
-  console.log(req.body);
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  res.send("");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
 
 app.listen(port, () => console.log("He he he he"));
