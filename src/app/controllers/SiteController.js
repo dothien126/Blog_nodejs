55a3d5f8807e68d6e0a15b1574beeda83aefd897
dothien126 <dothien2601ak39@gmail.com>
2022-03-05 21:49:39 +0700

    read data from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,19 @@
 const res = require('express/lib/response');
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-        Course.find({}, (err, courses) => {
-            if(!err) {
-                res.json(courses);
-            } else { 
-                res.status(400).json({error: 'Error!'});
-            }       
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next);
     }
     
-
     // [GET] /search
     search(req, res) {
         res.render('search');
---
+++ src/util/mongoose.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require("mongoose");
+
+module.exports = {
+    multipleMongooseToObject: (mongooses) => {
+        return mongooses.map(mongoose => mongoose.toObject());
+    },
+
+    mongooseToObject: (mongoose) => {
+        return mongoose ? mongoose.toObject() : mongoose;
+    }
+}
